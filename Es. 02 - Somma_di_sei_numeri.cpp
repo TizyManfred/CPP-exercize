e4dae79a9bf3f3499959fcb727b932d75c3c4d90
TizyManfred <97536032+TizyManfred@users.noreply.github.com>
2022-03-31 17:38:36 +0200

    Create Es. 02 - Somma_di_sei_numeri.cpp

---
+++ Es. 02 - Somma_di_sei_numeri.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num1,num2,num3,num4,num5;
+  cin>>num1>>num2>>num3>>num4>>num5;
+  int somma;
+  somma = num1+num2+num3+num4+num5;
+  cout<<"La somma è: "<<somma;
+  return 0;
+  
+} 
