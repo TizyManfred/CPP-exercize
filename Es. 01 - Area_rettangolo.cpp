99880c0f95da32c641f0e0a505ffde936beef2a1
TizyManfred <97536032+TizyManfred@users.noreply.github.com>
2022-03-31 17:37:41 +0200

    Create Es. 01 - Area_rettangolo.cpp

---
+++ Es. 01 - Area_rettangolo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float base;
+    float altezza;
+    cout<<"Inserisci la base"<<endl;
+    cin>>base;
+    cout<<"Inserisci l'altezza"<<endl;
+    cin>>altezza;
+    float area;
+    area = (base*altezza)/2;
+    cout<<"L'area è: "<<area;
+    return 0;
+}
